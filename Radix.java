cba9a6c359d4e85219388d60ee6b2c0bf6595dd0
wlin10 <wlin10@stuy.edu>
2021-01-14 00:24:15 -0500

    first

--- Radix.java
+++ Radix.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Radix{
 /*
 get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
@@ -7,7 +8,8 @@ nth(123,2) -> 1
 nth(-123,2) -> 1
 */
   public static int nth(int n, int col) {
-    return 0;
+    double ans = (n/(Math.pow(10.0, col)));
+    return ((int)(ans % 10));
   }
 /*
 return the number of digits in n.
@@ -17,7 +19,12 @@ length(-10) -> 2
 length(5112) -> 4
 */
   public static int length(int n) {
-    return 0;
+    int ans = (Integer.toString(n).length());
+    if (Integer.toString(n).charAt(0) == '-') {
+      return (ans - 1);
+    } else {
+      return ans;
+    }
   }
 /*
 This requires your MyLinkedList file to be present locally, but please do not commit this extra file.
@@ -28,7 +35,7 @@ Write and thoroughly test these methods!
 
 Up until now, there are no methods that care about the contents of your MyLinkedList. It does NOT matter that they are Strings.
 */
-  public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
+  public static void merge(MyLinkedList original, MyLinkedList[]buckets) {
 
   }
 
